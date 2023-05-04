bba287b7cd078d8c5f60fae595455bc253b04964
Scott Maxwell <maxwellscottj@gmail.com>
2023-05-04 12:13:19 -0700

    stringify form body data?

--- src/pages/users/loginForm.js
+++ src/pages/users/loginForm.js
@@ -50,7 +50,7 @@ class BareLoginForm extends react.Component {
 		
 		fetch(`${process.env.REACT_APP_NODE_URL}login`, {
 				method: "POST",
-				body: data,
+				body: JSON.stringify(data),
 				headers: { 'Content-Type': 'application/json' },
 				mode: 'cors',
 				}).then((response) => {
